45be98c752a6556fd4e438d2b1a857b45cff7213
kumar Devashish <kumardevashish000@gmail.com>
2024-06-28 23:12:38 +0530

    chore: Refactor ERC20Token contract, improve code readability, and add new functionalities

--- ERC20Token_project.sol
+++ ERC20Token_project.sol
@@ -4,14 +4,16 @@ pragma solidity ^0.8.10;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract ERC20Token is ERC20 {
-
     address private organizer;
     uint256 private totalDonations;
 
     mapping(address => uint256) private donations;
 
     modifier onlyOrganizer() {
-        require(msg.sender == organizer, "Only the organizer can perform this action");
+        require(
+            msg.sender == organizer,
+            "Only the organizer can perform this action"
+        );
         _;
     }
 
@@ -28,8 +30,11 @@ contract ERC20Token is ERC20 {
     function donateTokens(address to, uint256 amount) public {
         require(to != address(0), "Cannot donate to the zero address");
         require(amount > 0, "Donation amount must be greater than zero");
-        require(balanceOf(msg.sender) >= amount, "Insufficient balance to donate");
-        
+        require(
+            balanceOf(msg.sender) >= amount,
+            "Insufficient balance to donate"
+        );
+
         _transfer(msg.sender, to, amount);
         donations[msg.sender] += amount;
         totalDonations += amount;
@@ -37,8 +42,11 @@ contract ERC20Token is ERC20 {
 
     function burnTokens(uint256 amount) public {
         require(amount > 0, "Burn amount must be greater than zero");
-        require(balanceOf(msg.sender) >= amount, "Insufficient balance to burn");
-        
+        require(
+            balanceOf(msg.sender) >= amount,
+            "Insufficient balance to burn"
+        );
+
         _burn(msg.sender, amount);
     }
 
